cfdc2b70d528a216494b0195fbc2818f37fe32a6
123Hassif <hassif50@gmail.com>
2026-09-20 21:19:11 +0200

    Initial commit: Base Vite React TS setup with CI/CD

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Card-RPS - En cours de développement...</h1>
+      <p>Le pipeline CI/CD est opérationnel !</p>
+    </div>
+  )
+}
+
+export default App
